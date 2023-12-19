0e898469244973c7a187502dd6c9c08721da2690
lianhrc <androderrick@gmail.com>
2023-12-19 11:17:03 +0800

    inittial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home f/Home";
+import Products from "./pages/products f/Products";
+import Overview from "./pages/overview f/Overview";
+import Cart from "./pages/cart f/Cart";
+import "../src/pages/home f/Home.css"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element= {<Home/>}></Route>
+          <Route path="/products" element={<Products/>}></Route>
+          <Route path="/overview" element={<Overview/>}></Route>
+          <Route path="/cart" element={<Cart/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/home f/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () =>{
+  return (
+    <div className='pageContainer'>
+      <h1 className='home-header'>Home</h1>
+    </div>
+  )
+}
+
+export default Home
