f219a052165d57fc8bf6a97568b2b39ae9e405f6
lianhrc <androderrick@gmail.com>
2024-01-04 01:20:14 +0800

    section 1 of home page

--- frontend/src/pages/home f/Home.jsx
+++ frontend/src/pages/home f/Home.jsx
@@ -29,9 +29,26 @@ const Home = () =>{
             </div>
             <div className="introsection">
                  <div className="leftintrosection">
-                  
+                    <div className="left-one">
+                         <h1>ETHICALLY SOURCED,</h1>
+                    </div>
+                    <div className="left-two">
+                         <h1>LOCALLY GROWN</h1>
+                       
+                    </div>
+                    <div className="left-three"
+                    >
+                      <p>In the heart of the tropics, where sunlit fields unfold,<br/>Coffee beans, a treasure, in the soil, they're bold.<br/>Born of Philippine soil, nurtured by the sun's embrace,<br/>Each bean tells a story, a journey filled with grace.</p>
+                    </div>
+                    <div className="left-four">
+                          <button>Explore</button>
+                    </div>
                  </div>
-                 <div className="rightintrosection"></div>
+                 <div className="rightintrosection">
+                  <div className="rightimgcontainer">
+                    
+                  </div>
+                 </div>
             </div>
           </div>
           <div id='section2'>
